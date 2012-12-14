ae179e00773ab809158fb98adeb5b4f009d92c86
Noel <noel>
2012-12-14 23:49:33 +0000

    Passing unit tests

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -5,13 +5,24 @@ import service.{StackOverflowWebServiceProvider, StackOverflowProvider}
 import play.api.templates.{Template1, Html}
 
 class Application(provider: StackOverflowProvider,
+                  defaultUser: Int,
+                  threshold: Int,
                   positiveView: Template1[Int, Html],
                   negativeView: Template1[Int, Html]) extends Controller {
   
-  def index = Action {
-    Ok(positiveView.render(5))
-  }
+  def index = indexForUser(defaultUser)
 
+  def indexForUser(id: Int) = Action {
+    val score = provider.getScoreForUser(id)
+    println("Received score [%d] for user [%d]".format(score, id))
+    if(score < threshold) Ok(negativeView.render(score)) else Ok(positiveView.render(score))
+  }
 }
 
-object Application extends Application(StackOverflowWebServiceProvider, views.html.index, views.html.index)
+object Application extends Application(
+  StackOverflowWebServiceProvider,
+  1,
+  1000000,
+  views.html.index,
+  views.html.index
+)
--- test/controllers/ApplicationTest.scala
+++ test/controllers/ApplicationTest.scala
@@ -11,7 +11,7 @@ import play.api.mvc.Result
 class ApplicationTest extends Specification with Mockito {
 
   "The controller" should {
-    "put the correct value in the model for the default user" in {
+    "put the correct value in the model for the default user in the negative case" in {
 
       val defaultUser = 1
       val expectedScore = 100
@@ -23,17 +23,42 @@ class ApplicationTest extends Specification with Mockito {
       val mockNegativeView = mock[Template1[Int, Html]]
 
       mockPositiveView.render(anyInt) answers {i => Html(i.toString)}
+      mockNegativeView.render(anyInt) answers {i => Html(i.toString)}
 
-      val controller = new Application(mockProvider, mockPositiveView, mockNegativeView)
+      val controller = new Application(mockProvider, 1, 500, mockPositiveView, mockNegativeView)
       val responseFromController:Result = controller.index(FakeRequest())
 
       status(responseFromController) must equalTo (OK)
-      val response = contentAsString(responseFromController)
-      response must equalTo (expectedScore.toString)
+      contentAsString(responseFromController) must equalTo (expectedScore.toString)
 
       there was one(mockProvider).getScoreForUser(defaultUser)
       there was one(mockNegativeView).render(expectedScore)
       there was no(mockPositiveView).render(anyInt)
+    }
+
+    "put the correct value in the model for the default user in the positive case" in {
+
+      val defaultUser = 1
+      val expectedScore = 5000
+
+      val mockProvider = mock[StackOverflowProvider]
+      mockProvider.getScoreForUser(defaultUser) returns expectedScore
+
+      val mockPositiveView = mock[Template1[Int, Html]]
+      val mockNegativeView = mock[Template1[Int, Html]]
+
+      mockPositiveView.render(anyInt) answers {i => Html(i.toString)}
+      mockNegativeView.render(anyInt) answers {i => Html(i.toString)}
+
+      val controller = new Application(mockProvider, 1, 500, mockPositiveView, mockNegativeView)
+      val responseFromController:Result = controller.index(FakeRequest())
+
+      status(responseFromController) must equalTo (OK)
+      contentAsString(responseFromController) must equalTo (expectedScore.toString)
+
+      there was one(mockProvider).getScoreForUser(defaultUser)
+      there was one(mockPositiveView).render(anyInt)
+      there was no(mockNegativeView).render(expectedScore)
     }
   }
 }
